eadf3b469ddbb4795cb5ba78cbd31bf6072e8b48
Montezuma <valen02056@gmail.com>
2025-12-10 01:09:28 -0500

    Agregadas Cell y Player, extendidos Ship/Board, añadida logica inicial de la IA para posicionamiento automatico

--- Batalla-naval/src/main/java/edu/univalle/battleship/model/Player.java
+++ Batalla-naval/src/main/java/edu/univalle/battleship/model/Player.java
@@ -1,11 +1,42 @@
 package edu.univalle.battleship.model;
 
-public class Player {
-// Representa al jugador humano.
-// Tiene su Board y su flota.
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Representa al jugador humano.
+ * Contiene su tablero y su flota de barcos.
+ */
+public class Player implements Serializable {
 
-// placeShip(...): coloca barco mediante PositionController.
-// getBoard(): obtiene tablero.
-// getFleet(): lista de barcos.
+    private Board board;
+    private List<Ship> fleet;
+
+    public Player() {
+        this.board = new Board();
+        this.fleet = new ArrayList<>();
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public List<Ship> getFleet() {
+        return fleet;
+    }
+
+    /**
+     * Agrega un barco a la flota del jugador.
+     */
+    public void addShip(Ship ship) {
+        fleet.add(ship);
+    }
+
+    /**
+     * Devuelve true si todos los barcos del jugador estan hundidos.
+     */
+    public boolean hasLost() {
+        return board.allShipsSunk();
+    }
 }
